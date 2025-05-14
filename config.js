445c273be113acd3eb9edf68654a8530e69e35d0
navygod <tiagocorrea70@gmail.com>
2025-05-14 11:36:58 -0300

    Adiciona config.js para deploy no Netlify

---
+++ config.js
@@ -0,0 +1,4 @@
+window.CONFIG = {
+  PRODUCTS_SHEET_URL: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRKbHZihRiRJ4M_LZA-ENHV1OQlIP0leGk4uBPH6Dew2VLOFBJCRYe_UcR2CzPXefu9MtCJeEq9Wqo9/pub?gid=1473056083&single=true&output=csv',
+  DEPO_SHEET_URL: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRKbHZihRiRJ4M_LZA-ENHV1OQlIP0leGk4uBPH6Dew2VLOFBJCRYe_UcR2CzPXefu9MtCJeEq9Wqo9/pub?gid=1975500426&single=true&output=csv'
+};
